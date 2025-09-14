e6c963c40649a542b261fcd012ab265376e9bf6c
Pelayo Maojo <pelayo.mao@gmail.com>
2025-09-15 00:59:21 +0100

    feat(gui): add dashboard component

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import SimulatorControls from "./components/SimulatorControls";
-import ServiceManager from "./components/ServiceManager";
+import Dashboard from "./components/Dashboard";
 import RouteEditor from "./components/route_editor";
-import LogsView from "./components/logs_view";
-
-const Home: React.FC = () => (
-  <div>
-    <h1>MockForge GUI</h1>
-    <SimulatorControls />
-    <ServiceManager />
-    <LogsView />
-  </div>
-);
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/route_editor" element={<RouteEditor />} />
     </Routes>
   );
