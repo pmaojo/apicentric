8a79495426da29fb96e7c9524de34bcc06988631
Pelayo Maojo <pelayo.mao@gmail.com>
2025-09-14 18:33:21 +0100

    chore: remove test runner

--- src/context/init.rs
+++ src/context/init.rs
@@ -1,53 +1,8 @@
-use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use crate::config::PulseConfig;
-use crate::domain::ports::testing::{ChangeDetectorPort, RouteIndexerPort, WatcherPort};
-use crate::execution::ExecutionContext;
-use crate::TestRunnerPort;
 
-pub fn build_metrics_manager(
-    cfg: &PulseConfig,
-    exec_ctx: &ExecutionContext,
-) -> Arc<Mutex<crate::adapters::metrics::MetricsManager>> {
-    let mut metrics_manager = crate::adapters::metrics::MetricsManager::new();
-
-    if let Some(ref metrics_config) = cfg.metrics {
-        if let Some(ref allure_config) = metrics_config.allure {
-            if allure_config.enabled {
-                if let Ok(allure) = crate::adapters::metrics::AllureAdapter::new(PathBuf::from(
-                    &allure_config.report_dir,
-                )) {
-                    metrics_manager = metrics_manager.with_allure(allure);
-                }
-            }
-        }
-
-        if let Some(ref prometheus_config) = metrics_config.prometheus {
-            if prometheus_config.enabled && !exec_ctx.dry_run {
-                if let Ok(prom) =
-                    crate::adapters::metrics::PrometheusAdapter::new(prometheus_config.port)
-                {
-                    metrics_manager = metrics_manager.with_prometheus(prom);
-                }
-            }
-        }
-
-        if let Some(ref sentry_config) = metrics_config.sentry {
-            if sentry_config.enabled {
-                if let Ok(sentry) = crate::adapters::metrics::SentryAdapter::new(
-                    &sentry_config.dsn,
-                    &sentry_config.environment,
-                ) {
-                    metrics_manager = metrics_manager.with_sentry(sentry);
-                }
-            }
-        }
-    }
-
-    Arc::new(Mutex::new(metrics_manager))
-}
-
+/// Build the API simulator manager if enabled in configuration.
 pub fn build_api_simulator(
     cfg: &PulseConfig,
 ) -> Option<Arc<crate::simulator::ApiSimulatorManager>> {
@@ -67,46 +22,4 @@ pub fn build_api_simulator(
         }
     }
     None
-}
-
-pub fn build_adapters(
-    cfg: &PulseConfig,
-) -> (
-    Arc<dyn ChangeDetectorPort + Send + Sync>,
-    Arc<dyn RouteIndexerPort + Send + Sync>,
-    Arc<dyn TestRunnerPort + Send + Sync>,
-    Arc<crate::adapters::junit::JUnitAdapter>,
-    Arc<dyn WatcherPort + Send + Sync>,
-) {
-    let change_detector = Arc::new(crate::adapters::git::GitAdapter::new());
-    let route_indexer = Arc::new(
-        crate::adapters::route_indexer_adapter::RouteIndexerAdapter::new(Arc::new(
-            crate::adapters::route_indexer::RouteIndexer::new(
-                &cfg.routes_dir,
-                &cfg.specs_dir,
-                &cfg.index_cache_path,
-            ),
-        )),
-    );
-    let server_manager = Arc::new(crate::adapters::server_manager::ServerManager::new(
-        cfg.server.clone(),
-    ));
-    let test_runner = Arc::new(
-        crate::adapters::cypress::CypressAdapter::new(
-            cfg.cypress_config_path.clone(),
-            cfg.base_url.clone(),
-        )
-        .with_server_manager(
-            server_manager,
-            cfg.execution.mode.clone(),
-            cfg.server.clone(),
-        ),
-    );
-    let junit_adapter = Arc::new(crate::adapters::junit::JUnitAdapter::new(
-        cfg.reports_dir.clone(),
-    ));
-
-    let watcher = Arc::new(crate::adapters::watcher::FileWatcher::new(500)) as Arc<dyn WatcherPort + Send + Sync>;
-
-    (change_detector, route_indexer, test_runner, junit_adapter, watcher)
 }
--- src/domain/ports/mod.rs
+++ src/domain/ports/mod.rs
@@ -1,4 +1,3 @@
-pub mod testing;
 pub mod ui;
 pub mod system;
 pub mod contract;
