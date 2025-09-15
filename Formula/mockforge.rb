25914d029a7219d2499822a4a9a4d7c2590a7358
Pelayo Maojo <pelayo.mao@gmail.com>
2025-09-15 01:29:02 +0100

    chore: update mockforge sha256

--- Formula/mockforge.rb
+++ Formula/mockforge.rb
@@ -3,7 +3,7 @@ class Mockforge < Formula
   homepage "https://github.com/your-org/mockforge"
   version "0.1.0"
   url "https://github.com/your-org/mockforge/releases/download/v#{version}/mockforge-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # TODO: replace with real sha256
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # TODO: replace with real sha256
 
   def install
     bin.install "mockforge"
